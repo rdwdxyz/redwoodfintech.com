87222e654a0a82143b7e482251fc06569df50648
eizus <suzie@alxdr.ca>
2023-10-12 09:51:26 -0400

    First

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
